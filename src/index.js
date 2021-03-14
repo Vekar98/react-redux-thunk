e0128e22a4e2704d760d6edc7a0d476cf28f9673
Vekar98 <62066742+Vekar98@users.noreply.github.com>
2021-03-14 12:03:04 +0100

    example of using react redux thunk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootReducer = combineReducers({
+ ctr: counterReducer,
+ res: resultReducer
+})
+
+const logger = (store) => {
+ return next => {
+  return action => {
+   console.log('[Middleware] Dispatching', action);
+   const result = next(action)
+   console.log('[Middleware] next state', store.getState());
+   return result;
+  }
+ }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
---
+++ src/store/actions/index.js
@@ -0,0 +1,11 @@
+export {
+ add,
+ subtract,
+ increment,
+ decrement,
+} from './counter';
+
+export {
+ store_result,
+ deleteResult
+} from './result';
